02a29e45ceef8ca7e1a1da9564ffe7c69f74bc39
ludiazoctavio <ludiazoctavio@gmail.com>
2018-04-30 19:28:36 -0500

    LINES::STATIONS:: Se agregan los modelos para el despliegue en admin

--- apps/lines/admin.py
+++ apps/lines/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import LineModel, RouteModel
+
+
+class LineModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'color')
+    search_fields = ('name', 'key')
+
+admin.site.register(LineModel, LineModelAdmin)
+
+
+class RouteModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'line', 'stations', 'direction', 'is_active')
+    #search_fields = ('name', 'key')
+
+admin.site.register(RouteModel, RouteModelAdmin)
--- apps/stations/admin.py
+++ apps/stations/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import LocationModel, StationModel
+
+
+class LocationModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name', 'key')
+
+admin.site.register(LocationModel, LocationModelAdmin)
+
+
+class StationModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'location', 'order', 'is_active')
+    #search_fields = ('name', 'key')
+
+admin.site.register(StationModel, StationModelAdmin)
