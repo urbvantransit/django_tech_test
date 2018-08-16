bae060d6d1bfb9c6751833c593a51508649dad5b
Oribe Abad <subcero000@gmail.com>
2018-08-16 12:00:40 -0500

    add docstring for views of lines app

--- apps/lines/views.py
+++ apps/lines/views.py
@@ -5,25 +5,93 @@ from .serializers import LineSerializer,RouteSerializer
 from .models import LineModel,RouteModel
 from ..users.permissions import IsAdminUser,IsSuperAdminUser,IsNormalUser
 
+"""
+Views related to :model:`lines.LineModel` and
+:model:`lines.RouteModel`.
+"""
 class LineView(ListCreateView):
+    """
+    GET and POST for :model:`lines.LineModel`
+    child of ListCreateView related to Urbvan 
+    Framework.
+
+    **Context**
+
+    ``queryset``
+        Queryset of all :model:`lines.LineModel` objects
+    ``schema_class``
+        Schema of :model:`lines.LineModel`
+    ``serializer_class``
+        Serializer of :model:`lines.LineModel`
+    ``permission_classes``
+        Permission classes for get an post :model:`lines.LineModel`
+    """
     queryset = LineModel.objects.all()
     schema_class = LineSchema
     serializer_class = LineSerializer
     permission_classes = (IsNormalUser,)
 
 class LineUpdateView(UpdateView):
+    """
+    Update :model:`lines.LineModel`
+    child of UpdateView related to Urbvan 
+    Framework.
+
+    **Context**
+
+    ``queryset``
+        Queryset of all :model:`lines.LineModel` objects
+    ``schema_class``
+        Schema of :model:`lines.LineModel`
+    ``serializer_class``
+        Serializer of :model:`lines.LineModel`
+    ``permission_classes``
+        Permission classes for update  :model:`lines.LineModel`
+    """
     queryset = LineModel.objects.all()
     schema_class = LineSchema
     serializer_class = LineSerializer
     permission_classes = (IsAdminUser,)
 
 class LineDestroyView(DestroyView):
+    """
+    Delete :model:`lines.LineModel`
+    child of UpdateView related to Urbvan 
+    Framework.
+
+    **Context**
+
+    ``queryset``
+        Queryset of all :model:`lines.LineModel` objects
+    ``schema_class``
+        Schema of :model:`lines.LineModel`
+    ``serializer_class``
+        Serializer of :model:`lines.LineModel`
+    ``permission_classes``
+        Permission classes for delete a :model:`lines.LineModel`
+    """
     queryset = LineModel.objects.all()
     schema_class = LineSchema
     serializer_class = LineSerializer
     permission_classes = (IsSuperAdminUser,)
 
 class RouteView(ListCreateView):
+    """
+    GET and POST for :model:`lines.RouteModel`
+    child of ListCreateView related to Urbvan 
+    Framework.
+
+    **Context**
+
+    ``queryset``
+        Queryset of all :model:`lines.RouteModel` objects
+    ``schema_class``
+        Schema of :model:`lines.RouteModel`
+    ``serializer_class``
+        Serializer of :model:`lines.RouteModel`
+    ``permission_classes``
+        Permission classes for get an post :model:`lines.RouteModel`
+    """
     queryset = RouteModel.objects.all()
     schema_class = RouteSchema
     serializer_class = RouteSerializer
@@ -31,6 +99,22 @@ class RouteView(ListCreateView):
 
 
 class RouteUpdateView(UpdateView):
+    """
+    Update :model:`lines.RouteModel`
+    child of UpdateView related to Urbvan 
+    Framework.
+
+    **Context**
+
+    ``queryset``
+        Queryset of all :model:`lines.RouteModel` objects
+    ``schema_class``
+        Schema of :model:`lines.RouteModel`
+    ``serializer_class``
+        Serializer of :model:`lines.RouteModel`
+    ``permission_classes``
+        Permission classes for update  :model:`lines.RouteModel`
+    """
     queryset = RouteModel.objects.all()
     schema_class = RouteSchema
     serializer_class = RouteSerializer
@@ -38,6 +122,22 @@ class RouteUpdateView(UpdateView):
 
 
 class RouteDestroyView(DestroyView):
+    """
+    Delete :model:`lines.RouteModel`
+    child of UpdateView related to Urbvan 
+    Framework.
+
+    **Context**
+
+    ``queryset``
+        Queryset of all :model:`lines.RouteModel` objects
+    ``schema_class``
+        Schema of :model:`lines.RouteModel`
+    ``serializer_class``
+        Serializer of :model:`lines.RouteModel`
+    ``permission_classes``
+        Permission classes for delete a :model:`lines.RouteModel`
+    """
     queryset = RouteModel.objects.all()
     schema_class = RouteSchema
     serializer_class = RouteSerializer
