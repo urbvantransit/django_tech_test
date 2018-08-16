05338a6f36884d2610dae133eafc6a780e3c0fe0
Oribe Abad <subcero000@gmail.com>
2018-08-16 12:21:42 -0500

    fix permissions classes

--- apps/users/permissions.py
+++ apps/users/permissions.py
@@ -3,7 +3,7 @@ from rest_framework import permissions
 class IsNormalUser(permissions.BasePermission):
     
     def has_permission(self, request, view):
-        if request.user and request.user.profile.user_type == 0:
+        if request.user and request.user.profile.user_type >= 0:
             return True
         else:
             return False
@@ -11,7 +11,7 @@ class IsNormalUser(permissions.BasePermission):
 class IsAdminUser(permissions.BasePermission):
     
     def has_permission(self, request, view):
-        if request.user and request.user.profile.user_type == 1:
+        if request.user and request.user.profile.user_type >= 1:
             return True
         else:
             return False
