9c9a4ab75a0cd9c4418681930b34a88982f5eb85
Oribe Abad <subcero000@gmail.com>
2018-08-16 12:19:08 -0500

    add docstring of station app views

--- apps/stations/v1/views.py
+++ apps/stations/v1/views.py
@@ -4,34 +4,121 @@ from urbvan_framework.views import ListCreateView,UpdateView,DestroyView
 from .schemas import LocationSchema,StationSchema
 from .serializers import LocationSerializer,StationSerializer
 from ..models import LocationModel,StationModel
-
+"""
+Views related to :model:`stations.StationModel` and
+:model:`stations.LocationModel`.
+"""
 
 class LocationView(ListCreateView):
+    """
+    GET and POST for :model:`stations.LocationModel`
+    child of ListCreateView related to Urbvan 
+    Framework.
+
+    **Context**
+
+    ``queryset``
+        Queryset of all :model:`stations.LocationModel` objects
+    ``schema_class``
+        Schema of :model:`stations.LocationModel`
+    ``serializer_class``
+        Serializer of :model:`stations.LocationModel`
+    """
     queryset = LocationModel.objects.all()
     schema_class = LocationSchema
     serializer_class = LocationSerializer
 
 class LocationUpdateView(UpdateView):
+    """
+    Update `stations.LocationModel`
+    child of UpdateView related to Urbvan 
+    Framework.
+
+    **Context**
+
+    ``queryset``
+        Queryset of all `stations.LocationModel` objects
+    ``schema_class``
+        Schema of `stations.LocationModel`
+    ``serializer_class``
+        Serializer of `stations.LocationModel` 
+    """
     queryset = LocationModel.objects.all()
     schema_class = LocationSchema
     serializer_class = LocationSerializer
 
 class LocationDestroyView(DestroyView):
+    """
+    Delete `stations.LocationModel`
+    child of UpdateView related to Urbvan 
+    Framework.
+
+    **Context**
+
+    ``queryset``
+        Queryset of all `stations.LocationModel` objects
+    ``schema_class``
+        Schema of `stations.LocationModel`
+    ``serializer_class``
+        Serializer of `stations.LocationModel`
+    """
     queryset = LocationModel.objects.all()
     schema_class = LocationSchema
     serializer_class = LocationSerializer
 
 class StationView(ListCreateView):
+    """
+    GET and POST for :model:`stations.StationModel`
+    child of ListCreateView related to Urbvan 
+    Framework.
+
+    **Context**
+
+    ``queryset``
+        Queryset of all :model:`stations.StationModel` objects
+    ``schema_class``
+        Schema of :model:`stations.StationModel`
+    ``serializer_class``
+        Serializer of :model:`stations.StationModel`
+    """
     queryset = StationModel.objects.all()
     schema_class = StationSchema
     serializer_class = StationSerializer
 
 class StationUpdateView(UpdateView):
+    """
+    Update :model:`stations.StationModel`
+    child of UpdateView related to Urbvan 
+    Framework.
+
+    **Context**
+
+    ``queryset``
+        Queryset of all :model:`stations.StationModel` objects
+    ``schema_class``
+        Schema of :model:`stations.StationModel`
+    ``serializer_class``
+        Serializer of :model:`stations.StationModel` 
+    """
     queryset = StationModel.objects.all()
     schema_class = StationSchema
     serializer_class = StationSerializer
 
 class StationDestroyView(DestroyView):
+    """
+    Delete :model:`stations.StationModel`
+    child of UpdateView related to Urbvan 
+    Framework.
+
+    **Context**
+
+    ``queryset``
+        Queryset of all :model:`stations.StationModel` objects
+    ``schema_class``
+        Schema of :model:`stations.StationModel`
+    ``serializer_class``
+        Serializer of :model:`stations.StationModel`
+    """
     queryset = StationModel.objects.all()
     schema_class = StationSchema
     serializer_class = StationSerializer
