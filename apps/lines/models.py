80a1ef943afad3f956725637bbcdfd3797ee8de1
Oribe Abad <subcero000@gmail.com>
2018-08-16 11:26:42 -0500

    add docstrings to line and route model

--- apps/lines/models.py
+++ apps/lines/models.py
@@ -4,10 +4,14 @@ from django.db import models
 from apps.stations.models import StationModel
 
 from apps.utils import create_id
-
+"""
+This module is for declare LineModel and RouteModel,
+this classes are django.db models and are important for
+business.
+"""
 
 class LineModel(models.Model):
-
+    """LineModel class is child of django.db model"""
     id = models.CharField(default=create_id('line_'), primary_key=True,
                           max_length=30, unique=True)
     name = models.CharField(max_length=100)
@@ -15,7 +19,7 @@ class LineModel(models.Model):
 
 
 class RouteModel(models.Model):
-
+    """RouteModel class is child of django.db model"""
     id = models.CharField(default=create_id('route_'), primary_key=True,
                           max_length=30, unique=True)
     line = models.ForeignKey(LineModel, on_delete=models.DO_NOTHING)
