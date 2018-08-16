9fead18bced28421004c383c04c90ee461d60723
Oribe Abad <subcero000@gmail.com>
2018-08-15 22:14:07 -0500

    create permissions for NormalUser,Admin,SuperAdmin and implement in line and route views

--- apps/lines/views.py
+++ apps/lines/views.py
@@ -3,34 +3,42 @@ from urbvan_framework.views import ListCreateView,UpdateView,DestroyView
 from .schemas import LineSchema,RouteSchema
 from .serializers import LineSerializer,RouteSerializer
 from .models import LineModel,RouteModel
-
+from ..users.permissions import IsAdminUser,IsSuperAdminUser,IsNormalUser
 
 class LineView(ListCreateView):
     queryset = LineModel.objects.all()
     schema_class = LineSchema
     serializer_class = LineSerializer
+    permission_classes = (IsNormalUser,)
 
 class LineUpdateView(UpdateView):
     queryset = LineModel.objects.all()
     schema_class = LineSchema
     serializer_class = LineSerializer
+    permission_classes = (IsAdminUser,)
 
 class LineDestroyView(DestroyView):
     queryset = LineModel.objects.all()
     schema_class = LineSchema
     serializer_class = LineSerializer
+    permission_classes = (IsSuperAdminUser,)
 
 class RouteView(ListCreateView):
     queryset = RouteModel.objects.all()
     schema_class = RouteSchema
     serializer_class = RouteSerializer
+    permission_classes = (IsNormalUser,)
+
 
 class RouteUpdateView(UpdateView):
     queryset = RouteModel.objects.all()
     schema_class = RouteSchema
     serializer_class = RouteSerializer
+    permission_classes = (IsAdminUser,)
+
 
 class RouteDestroyView(DestroyView):
     queryset = RouteModel.objects.all()
     schema_class = RouteSchema
     serializer_class = RouteSerializer
+    permission_classes = (IsSuperAdminUser,)
---
+++ apps/users/permissions.py
@@ -0,0 +1,26 @@
+from rest_framework import permissions
+
+class IsNormalUser(permissions.BasePermission):
+    
+    def has_permission(self, request, view):
+        if request.user and request.user.profile.user_type == 0:
+            return True
+        else:
+            return False
+
+class IsAdminUser(permissions.BasePermission):
+    
+    def has_permission(self, request, view):
+        if request.user and request.user.profile.user_type == 1:
+            return True
+        else:
+            return False
+
+
+class IsSuperAdminUser(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.user and request.user.profile.user_type == 2:
+            return True
+        else:
+            return False
