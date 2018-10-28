96329373ad3f985e1ca6be8eee6ab5031c39ccc3
pamela <pamela@MacBook-Pro-de-CGM.local>
2018-10-28 10:43:51 -0600

    Identation

--- apps/stations/urls.py
+++ apps/stations/urls.py
@@ -4,9 +4,6 @@ from django.urls import path
 from .v1 import views as views_v1
 
 urlpatterns_v1_locations = ([
-
     path('',
-         views_v1.LocationView.as_view(),
-         name='v1_list_create_location'),
-
+        views_v1.LocationView.as_view(), name='v1_list_create_location'),
 ], 'locations')
