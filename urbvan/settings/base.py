477461259269435872929cde3d2ecf4b2482f494
pamela <pamela@MacBook-Pro-de-CGM.local>
2018-10-28 10:43:09 -0600

    Add config for data base

--- urbvan/settings/base.py
+++ urbvan/settings/base.py
@@ -85,10 +85,15 @@ WSGI_APPLICATION = 'urbvan.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# Change the permissions of your DB
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'urbvan',
+        'USER': 'roor',
+        'PASSWORD': 'r00t',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
