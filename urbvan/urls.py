d47e1dc897d135a6de7b5ff90dd6ae5a88e2e93b
Pablo Alejandro Salcedo Gonzalez <alex.salcedo.gonzalez@gmail.com>
2018-11-01 23:25:02 -0600

    feat(urbvan project): Add lines & routes urls paths

--- urbvan/urls.py
+++ urbvan/urls.py
@@ -5,10 +5,13 @@ from django.urls import (include, path)
 from rest_framework.authtoken import views
 
 from apps.stations.urls import urlpatterns_v1_locations
+from apps.lines.urls import urlpatterns_lines, urlpatterns_router
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-token-auth/', views.obtain_auth_token),
 
-    path('v1/locations/', include(urlpatterns_v1_locations))
+    path('v1/locations/', include(urlpatterns_v1_locations)),
+    path('v1/lines/', include(urlpatterns_lines)),
+    path('v1/routes/', include(urlpatterns_router))
 ]
