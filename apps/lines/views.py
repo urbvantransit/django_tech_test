3985ce6eab4b28abac6a58bd84c8bffacc4a80da
Pablo Alejandro Salcedo Gonzalez <alex.salcedo.gonzalez@gmail.com>
2018-11-01 23:23:57 -0600

    feat(lines): Add lines views

--- apps/lines/views.py
+++ apps/lines/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from urbvan_framework.views import ListCreateView,\
+                                                       UpdateView,\
+                                                       DestroyView,\
+                                                       ListAPIView
 
-# Create your views here.
+from .schemas import LineSchema, RouterSchema
+from .serializers import LineSerializer, RouteSerializer
+
+from .models import LineModel, RouteModel
+from ..users.permissions import BasicUser, StaffUser, SuperUser
+
+
+class LineView(ListCreateView):
+
+    queryset = LineModel.objects.order_by('id').all()
+    schema_class = LineSchema
+    serializer_class = LineSerializer
+    permission_classes = (StaffUser, )
+
+
+class LineUpdateView(UpdateView):
+
+    queryset = LineModel.objects.all()
+    schema_class = LineSchema
+    serializer_class = LineSerializer
+    permission_classes = (StaffUser, )
+
+
+class LineDestroyView(DestroyView):
+    queryset = LineModel.objects.all()
+    schema_class = LineSchema
+    serializer_class = LineSerializer
+    permission_classes = (SuperUser, )
+
+
+class RouterView(ListAPIView):
+
+    queryset = RouteModel.objects.order_by('id').all()
+    schema_class = RouterSchema
+    serializer_class = RouteSerializer
+    permission_classes = (BasicUser, )
