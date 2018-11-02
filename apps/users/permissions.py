40a618732196054942ef9d628a4da0ce8d5eac37
Pablo Alejandro Salcedo Gonzalez <alex.salcedo.gonzalez@gmail.com>
2018-11-01 23:11:00 -0600

    feat(users permissions): Add role to users to access resources

---
+++ apps/users/permissions.py
@@ -0,0 +1,20 @@
+from rest_framework.permissions import BasePermission
+
+
+class BasicUser(BasePermission):
+    """ Most basic user, only have access to read some data """
+
+    def has_permission(self, request, view):
+        return request.user
+
+
+class StaffUser(BasePermission):
+    """ Staff member, can get, create and update data """
+    def has_permission(self, request, view):
+        return request.user and request.user.is_staff
+
+
+class SuperUser(BasePermission):
+    """ Superuser has access to all the actions of the CRUD """
+    def has_permission(self, request, view):
+        return request.user and request.user.is_superuser
