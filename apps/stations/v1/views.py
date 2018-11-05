b098fcdd85c583d73235e62a6ac1ba3509e1f91b
pamela <pamela@MacBook-Pro-de-CGM.local>
2018-11-05 16:52:27 -0600

    CRUD para stations.locations

--- apps/stations/v1/views.py
+++ apps/stations/v1/views.py
@@ -1,5 +1,7 @@
 # coding: utf8
 from urbvan_framework.views import ListCreateView
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from .schemas import LocationSchema
 from .serializers import LocationSerializer
@@ -7,8 +9,14 @@ from .serializers import LocationSerializer
 from ..models import LocationModel
 
 
-class LocationView(ListCreateView):
-
+class LocationView(generics.ListCreateAPIView):
     queryset = LocationModel.objects.all()
     schema_class = LocationSchema
     serializer_class = LocationSerializer
+    permission_classes = (IsAuthenticated,)
+
+class LocationViewDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = LocationModel.objects.all()
+    schema_class = LocationSchema
+    serializer_class = LocationSerializer
+    permission_classes = (IsAuthenticated,)
