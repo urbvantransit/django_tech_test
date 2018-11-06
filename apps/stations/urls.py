a2636b9964e65d48a7361bfe1b25557ccb8217db
pamela <pamela@MacBook-Pro-de-CGM.local>
2018-11-06 14:11:59 -0600

    Agregando urls específicas para locations y stations

--- apps/stations/urls.py
+++ apps/stations/urls.py
@@ -1,12 +1,38 @@
 # coding: utf8
 from django.urls import path
 
-from .v1 import views as views_v1
+from .v1 import views
 
+# Creamos un router y registramos nuestros viewsets para locations
 urlpatterns_v1_locations = ([
+    # La siguiente url permite solamente GET y POST
+    path('', views.LocationView.as_view({
+        'get': 'list',
+        'post': 'create'
+    }), name='v1_list_create_location'),
 
-    path('',
-        views_v1.LocationView.as_view(), name='v1_list_create_location'),
-    path('<str:pk>/',
-        views_v1.LocationViewDetail.as_view(), name='v1_list_detail_location'),
+    # La siguiente url permite solamente GET y PUT, PATCH y DELETE
+    path('<str:pk>/', views.LocationView.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'patch': 'partial_update',
+        'delete': 'destroy'
+    }), name='v1_detail_location'),
 ], 'locations')
+
+# Creamos un router y registramos nuestros viewsets para stations
+urlpatterns_v1_stations = ([
+    # La siguiente url permite solamente GET y POST
+    path('', views.StationView.as_view({
+        'get': 'list',
+        'post': 'create'
+    }), name='v1_list_location'),
+
+    # La siguiente url permite solamente GET y PUT, PATCH y DELETE
+    path('<str:pk>/', views.StationView.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'patch': 'partial_update',
+        'delete': 'destroy'
+    }), name='v1_detail_location'),
+], 'stations')
