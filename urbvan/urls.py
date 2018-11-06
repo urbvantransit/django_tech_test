c840d4a17a396b77382e6f3409d2bdad66f76a67
pamela <pamela@MacBook-Pro-de-CGM.local>
2018-11-06 14:12:31 -0600

    Agregando include de urls para stations

--- urbvan/urls.py
+++ urbvan/urls.py
@@ -4,10 +4,14 @@ from django.urls import (include, path)
 
 from rest_framework.authtoken import views
 
-from apps.stations.urls import urlpatterns_v1_locations
+from apps.stations.urls import (
+    urlpatterns_v1_locations,
+    urlpatterns_v1_stations
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-token-auth/', views.obtain_auth_token),
-    path('v1/locations/', include(urlpatterns_v1_locations))
+    path('v1/locations/', include(urlpatterns_v1_locations)),
+    path('v1/stations/', include(urlpatterns_v1_stations))
 ]
