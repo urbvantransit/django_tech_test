ca566c4a92904487e85610a5c8731f871fccb2cb
pamela <pamela@MacBook-Pro-de-CGM.local>
2018-11-06 14:13:16 -0600

    Agreando Schemas para el modelo de Stations

--- apps/stations/v1/schemas.py
+++ apps/stations/v1/schemas.py
@@ -3,8 +3,13 @@ from marshmallow import (Schema, fields)
 
 
 class LocationSchema(Schema):
-
     id = fields.String()
     name = fields.String()
     latitude = fields.Decimal()
     longitude = fields.Decimal()
+
+class StationSchema(Schema):
+    id = fields.String()
+    order = fields.Integer()
+    location = fields.Integer()
+    is_active = fields.Boolean()    
