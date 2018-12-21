bc7e06e497ea8f27396c73d80d509d60079c8bf2
rtobon <tobon0412@gmail.com>
2018-12-20 23:13:56 -0600

    Add urls to stations, lines and routes services

--- urbvan/urls.py
+++ urbvan/urls.py
@@ -4,12 +4,13 @@ from django.urls import (include, path)
 
 from rest_framework.authtoken import views
 from apps.lines import urls
-from apps.stations.urls import urlpatterns_v1_locations
+from apps.stations.urls import urlpatterns_v1_locations, urls_stations
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-token-auth/', views.obtain_auth_token),
     path('v1/locations/', include(urlpatterns_v1_locations)),
-    path('lines/', include(urls))
+    path('api/v1/', include(urls_stations)),
+    path('api/v1/', include(urls))
 
 ]
