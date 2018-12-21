e31db4cb955af04e7a1b88fdd5f956b3ab1dd4f0
nas-rtobon <rtobon@nasolutions.mx>
2018-12-21 14:42:48 -0600

    Add staff and superadmin users factory

--- apps/users/factories.py
+++ apps/users/factories.py
@@ -18,6 +18,31 @@ class UserFactory(factory.django.DjangoModelFactory):
     is_active = True
 
 
+class UserStaffFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = User
+
+    username = factory.Faker('user_name')
+    email = factory.Faker('safe_email')
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    is_staff = True
+    is_active = True
+
+
+class UserSuperadminFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = User
+
+    username = factory.Faker('user_name')
+    email = factory.Faker('safe_email')
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    is_superuser = True
+    is_active = True
+
 class TokenFactory(factory.django.DjangoModelFactory):
 
     class Meta:
