237851c30e87b7a2f6924979eeea58cef973a5c3
paula-em-lafon <paula.em.lafon@gmail.com>
2019-04-14 16:01:06 -0600

    custom urbvan generic views createdd

--- urbvan_framework/views.py
+++ urbvan_framework/views.py
@@ -1,6 +1,7 @@
 # coding: utf8
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.mixins import RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 
 from .mixins import (CreateModelMixin, ListModelMixin)
 from .schemas import PaginationResponse
@@ -34,3 +35,36 @@ class ListAPIView(ListModelMixin, GenericAPIView):
 
 class ListCreateView(CreateAPIView, ListAPIView):
     pass
+
+class RetrieveAPIView(RetrieveModelMixin, GenericAPIView):
+    """
+    Concrete view for retrieving a model instance.
+    """
+    authentication_classes = (CustomTokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+class UpdateAPIView(UpdateModelMixin, GenericAPIView):
+    """
+    Concrete view for updating a model instance.
+    """
+    authentication_classes = (CustomTokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+class DestroyApiView(DestroyModelMixin, GenericAPIView):
+    """
+    Concrete view for destroying a model instance.
+    """
+    authentication_classes = (CustomTokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
