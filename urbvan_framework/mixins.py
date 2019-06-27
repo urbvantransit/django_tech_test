8bcce25d1cc8202ad636b9406a3a4276b25c0646
Alex Maguey <amaguey@rappi.com>
2019-06-27 01:55:28 -0500

    ✨ Add mixins to complete CRUD functionality
    We don't add a custom delete mixin since it won't use our schema class,
    it will be enough with the DRF mixin.

--- urbvan_framework/mixins.py
+++ urbvan_framework/mixins.py
@@ -43,8 +43,48 @@ class ListModelMixin(object):
         queryset = self.filter_queryset(queryset)
 
         page = self.paginate_queryset(queryset)
-        schema = self.schema_class(many=True)
 
+        schema = self.schema_class(many=True)
         schema = schema.dump(page).data
 
         return self.get_paginated_response(schema)
+
+
+class RetrieveModelMixin:
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        schema = self.schema_class()
+        schema = schema.dump(instance).data
+
+        response = render_to_response(body=schema)
+
+        return Response(response)
+
+
+class UpdateModelMixin:
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        schema = self.schema_class()
+        schema = schema.dump(instance).data
+        response = render_to_response(body=schema)
+        return Response(response)
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+    def partial_update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
