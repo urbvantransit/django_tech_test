292e17ade96a45dd3abc77def935fcbd49a5769f
Leonardo Alonso <leonardoalonsososa@gmail.com>
2019-07-11 17:16:18 -0500

    Vistas de detalle en Station y Location

--- apps/stations/urls.py
+++ apps/stations/urls.py
@@ -8,6 +8,9 @@ urlpatterns_v1_locations = ([
     path('',
          views_v1.LocationView.as_view(),
          name='v1_list_create_location'),
+    path('<str:pk>', 
+        views_v1.LocationDetailView.as_view(),
+        name="v1_detail_location")
 
 ], 'locations')
 
@@ -16,5 +19,8 @@ urlpatterns_v1_stations = ([
     path('',
          views_v1.StationView.as_view(),
          name='v1_list_create_station'),
+    path('<str:pk>', 
+        views_v1.StationDetailView.as_view(), 
+        name="v1_detail_stations"),
 
 ], 'station')
--- apps/stations/v1/views.py
+++ apps/stations/v1/views.py
@@ -1,6 +1,6 @@
 # coding: utf8
 from urbvan_framework.views import ListCreateView
-
+from rest_framework import generics, mixins
 from .schemas import LocationSchema, StationSchema
 from .serializers import LocationSerializer, StationSerializer
 
@@ -13,9 +13,48 @@ class LocationView(ListCreateView):
     schema_class = LocationSchema
     serializer_class = LocationSerializer
 
+'''
+Vista de detalle para Locaton
 
+Se agrega funcionalidad para eliminar y actualizar en base al id
+'''
+class LocationDetailView(mixins.DestroyModelMixin, mixins.UpdateModelMixin, generics.RetrieveAPIView):
+    authentication_classes = (CustomTokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    serializer_class = LocationSerializer
+    queryset = LocationModel.objects.all()
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, *kwargs)
+    
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, *kwargs)
+
+
+'''
+Listar Staciones
+
+Se agrega funconalidad para crear nuevas estaciones
+'''
 class StationView(ListCreateView):
 
     queryset = StationModel.objects.all()
     schema_class = StationSchema
     serializer_class = StationSerializer
+
+'''
+Vista de detalle Station
+
+Se agrega funcionalidad para eliminar, actualizar y ver el detalle en base al id
+'''
+class StationDetailView(mixins.DestroyModelMixin, mixins.UpdateModelMixin, generics.RetrieveAPIView):
+    authentication_classes = (CustomTokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    serializer_class = StationSerializer
+    queryset = StationModel.objects.all()
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, *kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, *kwargs)
