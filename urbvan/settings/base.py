6bfe851d47fb9934fc61e3d366a67ba4436691b3
Leonardo Alonso <leonardoalonsososa@gmail.com>
2019-07-11 16:46:10 -0500

    Agregar valores default para paginacion

--- urbvan/settings/base.py
+++ urbvan/settings/base.py
@@ -136,5 +136,7 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
