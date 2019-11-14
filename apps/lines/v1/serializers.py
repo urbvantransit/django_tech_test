c3a8310fbe55d03a835d297955bb60f84795210a
tobon0412 <tobon0412@gmail.com>
2019-11-14 11:47:53 -0600

    Add Serializer for Route model

--- apps/lines/v1/serializers.py
+++ apps/lines/v1/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from apps.lines.models import LineModel
+from apps.lines.models import LineModel, RouteModel
 
 
 class LineModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = LineModel
         fields = ("id", "name", "color")
+
+
+class RouteModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RouteModel
+        fields = ("line", "stations", "direction", "is_active")
