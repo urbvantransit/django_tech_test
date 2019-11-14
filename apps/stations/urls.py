68dcae371f064fd7f6367ffea37193a96f3a6548
tobon0412 <tobon0412@gmail.com>
2019-11-14 12:40:09 -0600

    Add urls for Station model

--- apps/stations/urls.py
+++ apps/stations/urls.py
@@ -1,12 +1,26 @@
 # coding: utf8
 from django.urls import path
-
 from .v1 import views as views_v1
+from .v1.views import (
+    StationModelListView,
+    StationModelDetailView,
+    StationModelDeleteView,
+    StationModelCreateView,
+)
 
-urlpatterns_v1_locations = ([
+urlpatterns_v1_locations = (
+    [path("", views_v1.LocationView.as_view(), name="v1_list_create_location"),],
+    "locations",
+)
 
-    path('',
-         views_v1.LocationView.as_view(),
-         name='v1_list_create_location'),
-
-], 'locations')
+urls_stations = (
+    [
+        path("", StationModelListView.as_view(), name="stations-all"),
+        path("<str:pk>/", StationModelDetailView.as_view(), name="stations-detail",),
+        path("create", StationModelCreateView.as_view(), name="stations-create"),
+        path(
+            "delete/<str:pk>", StationModelDeleteView.as_view(), name="stations-delete",
+        ),
+    ],
+    "stations",
+)
