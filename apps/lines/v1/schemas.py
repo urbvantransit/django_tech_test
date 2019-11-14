b5cd5979fb5484f3147ebf5872210237a18cdab4
tobon0412 <tobon0412@gmail.com>
2019-11-14 11:47:19 -0600

    Add Schema for Route model

--- apps/lines/v1/schemas.py
+++ apps/lines/v1/schemas.py
@@ -1,5 +1,6 @@
 # coding: utf8
-from marshmallow import Schema, fields
+from marshmallow import (Schema, fields)
+from apps.stations.v1.schemas import StationSchema
 
 
 class LinesSchema(Schema):
@@ -7,3 +8,12 @@ class LinesSchema(Schema):
     id = fields.String()
     name = fields.String()
     color = fields.Decimal()
+
+
+class RouteSchema(Schema):
+
+    id = fields.String()
+    line = fields.String()
+    stations = fields.Nested(StationSchema, many=True, required=False)
+    direction = fields.Boolean()
+    is_active = fields.Boolean()
