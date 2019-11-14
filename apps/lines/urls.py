3d2e18a3b05b537aebead335b5ad7bce332a6b64
tobon0412 <tobon0412@gmail.com>
2019-11-14 12:22:39 -0600

    Add urls for Route model

--- apps/lines/urls.py
+++ apps/lines/urls.py
@@ -5,6 +5,10 @@ from .v1.views import (
     LineModelDetailView,
     LineModelCreateView,
     LineModelDeleteView,
+    RouteModelDetailView,
+    RouteModelListView,
+    RouteModelCreateView,
+    RouteModelDeleteView,
 )
 
 urlpatterns_lines = [
@@ -12,4 +16,12 @@ urlpatterns_lines = [
     path("<str:pk>/", LineModelDetailView.as_view(), name="line-detail"),
     path("create", LineModelCreateView.as_view(), name="line-create"),
     path("lines/delete/<str:pk>", LineModelDeleteView.as_view(), name="line-delete"),
+]
+
+
+urlpatterns_routes = [
+    path("", RouteModelListView.as_view(), name="routes-all"),
+    path("<str:pk>/", RouteModelDetailView.as_view(), name="routes-detail"),
+    path("create", RouteModelCreateView.as_view(), name="routes-create"),
+    path("delete/<str:pk>", RouteModelDeleteView.as_view(), name="routes-delete"),
 ]
