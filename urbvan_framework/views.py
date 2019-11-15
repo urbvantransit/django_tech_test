a0fc7224068c722e0e2b8d69ab998b685855c8f1
tobon0412 <tobon0412@gmail.com>
2019-11-14 18:16:34 -0600

    Add Permissions for Staff, SuperUser and Anonymous

--- urbvan_framework/views.py
+++ urbvan_framework/views.py
@@ -1,6 +1,6 @@
 # coding: utf8
 from rest_framework.generics import GenericAPIView
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, BasePermission
 
 from .mixins import (CreateModelMixin, ListModelMixin)
 from .schemas import PaginationResponse
@@ -34,3 +34,29 @@ class ListAPIView(ListModelMixin, GenericAPIView):
 
 class ListCreateView(CreateAPIView, ListAPIView):
     pass
+
+
+class IsSuperUser(BasePermission):
+    """
+    Allows access only to superusers for (CRUD)
+    """
+    def has_permission(self, request, view):
+        return request.user.is_superuser
+
+
+class IsAnonymousUser(BasePermission):
+    """
+    Allows access only to  users only for read
+    """
+    def has_permission(self, request, view):
+        return request.user.is_anonymous or request.user.is_superuser or request.user.is_staff
+
+
+class IsStaffUser(BasePermission):
+    """
+    Allows access only to staff users for update models
+
+    """
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_superuser or request.user.is_staff
