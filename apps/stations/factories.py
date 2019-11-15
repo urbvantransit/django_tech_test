c9f274c58458d46677370736fc3c9ea2b1a37d9a
tobon0412 <tobon0412@gmail.com>
2019-11-14 18:21:32 -0600

    Add Factory for test of line, routes and stations

--- apps/stations/factories.py
+++ apps/stations/factories.py
@@ -1,14 +1,25 @@
 # coding: utf8
 import factory
+from faker import Factory
+from .models import LocationModel, StationModel
 
-from .models import LocationModel
+faker = Factory.create()
 
 
 class LocationFactory(factory.django.DjangoModelFactory):
-
     class Meta:
         model = LocationModel
 
-    name = factory.Faker('slug')
-    latitude = factory.Faker('latitude')
-    longitude = factory.Faker('longitude')
+    name = factory.Faker("slug")
+    latitude = factory.Faker("latitude")
+    longitude = factory.Faker("longitude")
+
+
+class StationFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = StationModel
+
+    location = factory.SubFactory(LocationFactory)
+    order = faker.random_number()
+    is_active = True
