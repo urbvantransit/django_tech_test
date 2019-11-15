8a4e97a4d705792e823d2a873e54af9c32c243ec
tobon0412 <tobon0412@gmail.com>
2019-11-14 22:16:22 -0600

    Add factories for Users Permissions

--- apps/users/factories.py
+++ apps/users/factories.py
@@ -1,25 +1,48 @@
 # coding: utf8
-import factory
-
 from django.contrib.auth.models import User
-
 from rest_framework.authtoken.models import Token
+
+import factory
 
 
 class UserFactory(factory.django.DjangoModelFactory):
-
     class Meta:
         model = User
 
-    username = factory.Faker('user_name')
-    email = factory.Faker('safe_email')
-    first_name = factory.Faker('first_name')
-    last_name = factory.Faker('last_name')
+    username = factory.Faker("user_name")
+    email = factory.Faker("safe_email")
+    first_name = factory.Faker("first_name")
+    last_name = factory.Faker("last_name")
+    is_active = True
+
+
+class UserStaffFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = factory.Faker("user_name")
+    email = factory.Faker("safe_email")
+    password = factory.Faker("password")
+    first_name = factory.Faker("first_name")
+    last_name = factory.Faker("last_name")
+    is_staff = True
+    is_active = True
+
+
+class UserSuperadminFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = factory.Faker("user_name")
+    email = factory.Faker("safe_email")
+    password = factory.Faker("password")
+    first_name = factory.Faker("first_name")
+    last_name = factory.Faker("last_name")
+    is_superuser = True
     is_active = True
 
 
 class TokenFactory(factory.django.DjangoModelFactory):
-
     class Meta:
         model = Token
-        django_get_or_create = ('user',)
+        django_get_or_create = ("user",)
