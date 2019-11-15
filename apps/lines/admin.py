cfb8285ab73a28225bb5df57f205159516c569dd
migantoju <a_niupi_byy@hotmail.com>
2019-11-15 15:45:50 -0600

    Se hizo la migración y se añadieron los modelos al admin

--- apps/lines/admin.py
+++ apps/lines/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from .models import LineModel, RouteModel
+
 # Register your models here.
+@admin.register(LineModel)
+class LineModelAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'name', 'color', )
+    ordering = ('id', 'name', 'color', )
+    search_fields = ('name', )
+
+# @admin.register(RouteModel)
+# class RouteModelAdmin(admin.ModelAdmin):
+
+#     list_display = ('id', 'line', 'station', 'directions', 'is_active', )
+#     ordering = ('is_active', )
+#     search_fields = ('is_active',)
+admin.site.register(RouteModel)
--- apps/stations/admin.py
+++ apps/stations/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from apps.stations.models.locations import LocationModel
+from apps.stations.models.stations import StationModel
 # Register your models here.
+
+admin.site.register(LocationModel)
+admin.site.register(StationModel)
