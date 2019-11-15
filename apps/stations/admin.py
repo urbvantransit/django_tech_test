0d705f07eaa0ba4432a347b2889330ffb587d26a
tobon0412 <tobon0412@gmail.com>
2019-11-14 18:29:18 -0600

    Add Admin for the historical

--- apps/stations/admin.py
+++ apps/stations/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from apps.stations.models.stations import Historical
 
 # Register your models here.
+
+
+class HistoricalAdmin(admin.ModelAdmin):
+    fields = ("event", "model_type", "created_at", "updated_at")
+    readonly_fields = ("created_at", "updated_at")
+    list_display = ("event", "model_type", "created_at", "updated_at")
+
+
+admin.site.register(Historical, HistoricalAdmin)
