f3462a3cf98203cd497deeae59051c98399a5e0d
tobon0412 <tobon0412@gmail.com>
2019-11-14 20:40:43 -0600

    Add collection with the stations, routes and lines API

--- apps/stations/models/stations.py
+++ apps/stations/models/stations.py
@@ -1,6 +1,6 @@
 # coding: utf8
 from django.db import models
-from django.db.models.signals import post_delete
+from django.db.models.signals import post_delete, pre_save
 from .locations import LocationModel
 from apps.utils import create_id
 
