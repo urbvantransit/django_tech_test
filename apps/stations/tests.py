42b31da560dd647e2d43a0c14d9bd5955ec332c1
tobon0412 <tobon0412@gmail.com>
2019-11-14 18:22:53 -0600

    Add Test for Stations

--- apps/stations/tests.py
+++ apps/stations/tests.py
@@ -1,10 +1,114 @@
 # coding: utf8
 from django.urls import reverse
-
 from rest_framework.test import APITestCase
+from apps.users.factories import (
+    UserFactory,
+    UserStaffFactory,
+    UserSuperadminFactory,
+    TokenFactory,
+)
+from apps.stations.factories import LocationFactory, StationFactory
 
-from apps.users.factories import (UserFactory, TokenFactory)
-from apps.stations.factories import LocationFactory
+
+class StationCreateTest(APITestCase):
+
+    url_list = reverse("stations:stations-all")
+    url_create = reverse("stations:stations-create")
+    url_detail = reverse("stations:stations-detail", kwargs={"pk": "pk"})
+    url_delete = reverse("stations:stations-delete", kwargs={"pk": "pk"})
+
+    def setUp(self):
+        self.user = UserFactory()
+        self.user_token = TokenFactory(user=self.user)
+
+        self.client.credentials(
+            HTTP_AUTHORIZATION="Urbvan {}".format(self.user_token.key)
+        )
+
+    def test_list(self):
+        StationFactory()
+
+        response = self.client.get(self.url_list)
+        response = response.json()
+
+        self.assertEquals(len(response), 1)
+
+    def test_create_successfully(self):
+        self.user = UserSuperadminFactory()
+        self.user_token = TokenFactory(user=self.user)
+
+        self.client.credentials(
+            HTTP_AUTHORIZATION="Urbvan {}".format(self.user_token.key)
+        )
+        location = LocationFactory()
+        data = {"order": 1, "is_active": True, "location": location.id}
+
+        response = self.client.post(self.url_create, data, format="json")
+        self.assertEquals(response.status_code, 201)
+
+    def test_create_forbidden(self):
+        self.user = UserFactory()
+        self.user_token = TokenFactory(user=self.user)
+
+        self.client.credentials(
+            HTTP_AUTHORIZATION="Urbvan {}".format(self.user_token.key)
+        )
+        location = LocationFactory()
+        data = {"order": 1, "is_active": True, "location": location.id}
+
+        response = self.client.post(self.url_create, data, format="json")
+        self.assertEquals(response.status_code, 403)
+
+    def test_detail_successfully(self):
+        self.user = UserStaffFactory()
+        self.user_token = TokenFactory(user=self.user)
+
+        self.client.credentials(
+            HTTP_AUTHORIZATION="Urbvan {}".format(self.user_token.key)
+        )
+        station = StationFactory()
+        data = {"pk": station.id}
+        url = str(self.url_detail).replace("pk", data["pk"])
+        response = self.client.get(url, data, format="json")
+        self.assertEquals(response.status_code, 200)
+
+    def test_detail_forbidden(self):
+        self.user = UserFactory()
+        self.user_token = TokenFactory(user=self.user)
+
+        self.client.credentials(
+            HTTP_AUTHORIZATION="Urbvan {}".format(self.user_token.key)
+        )
+        station = StationFactory()
+        data = {"pk": station.id}
+        url = str(self.url_detail).replace("pk", data["pk"])
+        response = self.client.get(url, data, format="json")
+        self.assertEquals(response.status_code, 403)
+
+    def test_delete_successfully(self):
+        self.user = UserSuperadminFactory()
+        self.user_token = TokenFactory(user=self.user)
+
+        self.client.credentials(
+            HTTP_AUTHORIZATION="Urbvan {}".format(self.user_token.key)
+        )
+        station = StationFactory()
+        data = {"pk": station.id}
+
+        # Get with success the element
+        url = str(self.url_detail).replace("pk", data["pk"])
+        response = self.client.get(url, data, format="json")
+        self.assertEquals(response.status_code, 200)
+
+        # Delete element and check
+        url = str(self.url_delete).replace("pk", data["pk"])
+        response = self.client.delete(url, data, format="json")
+        self.assertEquals(response.status_code, 204)
+
+        # Not found element
+        url = str(self.url_detail).replace("pk", data["pk"])
+        response = self.client.get(url, data, format="json")
+        self.assertEquals(response.status_code, 404)
 
 
 class LocationCreateTest(APITestCase):
@@ -25,14 +129,10 @@ class LocationCreateTest(APITestCase):
         response = self.client.get(self.url)
         response = response.json()
 
-        self.assertEquals(response['body'].get('count'), 1)
+        self.assertEquals(response["body"].get("count"), 1)
 
     def test_create_successfully(self):
-        data = {
-            "name": "Urbvan",
-            "latitude": 19.388401,
-            "longitude": -99.227358
-        }
+        data = {"name": "Urbvan", "latitude": 19.388401, "longitude": -99.227358}
 
-        response = self.client.post(self.url, data, format='json')
+        response = self.client.post(self.url, data, format="json")
         self.assertEquals(response.status_code, 201)
