b4f35f8a51219b872482d23600487906effdb5a7
migantoju <a_niupi_byy@hotmail.com>
2019-11-16 02:11:25 -0600

    Intenté seguir el ejemplo y hacer la API tal cual como se encontró originalmente en el repo, pero no lo logré al 100%, así que regresé a como yo sé hacerlo para no perder el tiempo

--- urbvan_framework/mixins.py
+++ urbvan_framework/mixins.py
@@ -49,3 +49,48 @@ class ListModelMixin(object):
         schema = schema.dump(page).data
 
         return self.get_paginated_response(schema)
+
+class RetrieveModelMixin(object):
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        schema = self.schema_class()
+        schema = schema.dump(instance).data
+        response = render_to_response(body=schema)
+        return Response(response, status=status.HTTP_200_OK)
+
+class UpdateModelMixin(mixins.UpdateModelMixin):
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        if not serializer.is_valid():
+            response = render_response_error(errors=serializer.errors)
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            self.perform_update(serializer)
+        except Exception as e:
+            if hasattr(e, 'detail'):
+                error = e.detail
+            else:
+                error = {"base": {"message": str(e)}}
+
+            response = render_response_error(errors=error)
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+        if getattr(instance, '_prefetched_objects_cache', None):
+            instance._prefetched_objects_cache = {}
+        instance = serializer.instance
+        schema = self.schema_class()
+        schema = schema.dump(instance).data
+        response = render_to_response(body=schema)
+        #return Response(serializer.data)
+        return Response(response, status=status.HTTP_200_OK)
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+    def partial_update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
--- urbvan_framework/views.py
+++ urbvan_framework/views.py
@@ -1,9 +1,15 @@
 # coding: utf8
 from rest_framework.generics import GenericAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import mixins
 
 from .authentication import CustomTokenAuthentication
-from .mixins import CreateModelMixin, ListModelMixin
+from .mixins import (
+                    CreateModelMixin, 
+                    ListModelMixin, 
+                    RetrieveModelMixin,
+                    UpdateModelMixin,
+                )
 from .schemas import PaginationResponse
 
 
@@ -34,3 +40,36 @@ class ListAPIView(ListModelMixin, GenericAPIView):
 
 class ListCreateView(CreateAPIView, ListAPIView):
     pass
+
+class RetrieveView(RetrieveModelMixin, GenericAPIView):
+    """
+    Vista concreta para obtener un objeto, en este caso 
+    una `Line` pasandole el id de la linea.
+    """
+
+    authentication_classes = (CustomTokenAuthentication, )
+    permission_classes = (IsAuthenticated, )
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+class UpdateView(UpdateModelMixin, GenericAPIView):
+
+    authentication_classes = (CustomTokenAuthentication, )
+    permission_classes = (IsAuthenticated, )
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+class DestroyView(mixins.DestroyModelMixin,
+                     GenericAPIView):
+    authentication_classes = (CustomTokenAuthentication, )
+    permission_classes = (IsAuthenticated, )
+    """
+    Concrete view for deleting a model instance.
+    """
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
