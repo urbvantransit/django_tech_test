9f9599006791e65f49bd988b340e6253018f01e7
migantoju <a_niupi_byy@hotmail.com>
2019-11-15 19:48:23 -0600

    se creó una apps de nombre core, en la cual se encuentran archivos con funciones que se pueden reutilizar en cualquier otra aplicación

---
+++ apps/core/models.py
@@ -0,0 +1,14 @@
+from django.db import models 
+
+class TimeStampedModel(models.Model):
+    """
+    Un modelo clase que nos provee de un campo
+    `createdAt` & `updatedAt` en cualquier modelo hijo
+    del cual herede éste.
+    """
+    createdAt = models.DateTimeField('Fecha de Creación', auto_now_add=True)
+    updatedAt = models.DateTimeField('Fecha de Actualización', auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ('-createdAt', )
---
+++ apps/core/permissions.py
@@ -0,0 +1,33 @@
+# Aquí se almacenan los decoradores necesarios para añadir
+# una capa de seguridad a las vistas/endpoints de la api
+# estos decoradores añaden permisos a los endpoints,
+# en caso de que un usuario no pase la prueba con 
+# `user_passes_test`, retorna `False` y por ende, lo rechaza
+# y quiere decir que no tiene permisos para hacer x o y acción
+
+from django.contrib.auth.decorators import user_passes_test
+from django.core.exceptions import PermissionDenied
+
+def group_required(*group_name):
+    """
+    Requiere que para hacer la acción que se pretende,
+    el usuario debe estar en un grupo específico.
+    """
+    def in_groups(u):
+        if u.is_authenticated():
+            if bool(u.groups.filter(name__in=group_name)) | u.is_superuser:
+                return True
+        return False
+    return user_passes_test(in_groups) 
+
+
+def superuser_only(function):
+    """
+    Lmita la vista únicamente a los super usuarios
+    """
+
+   def _inner(request, *args, **kwargs):
+       if not request.user.is_superuser:
+           raise PermissionDenied           
+       return function(request, *args, **kwargs)
+   return _inner
